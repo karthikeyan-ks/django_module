3fa4e497508ff8a7dfdd68541617f5087e77cc85
karthikeyan-ks <karthikeyanks3673@gmail.com>
2025-04-12 16:29:55 +0000

    aws change

--- channels_wrapper/consumers/chat/base.py
+++ channels_wrapper/consumers/chat/base.py
@@ -5,7 +5,10 @@ class BaseConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
         print(f"Connected with channel name: {self.channel_name}")
-        
+        await self.send(text_data="Connected to WebSocket!")
         
     async def disconnect(self, code):
         return await super().disconnect(code)
+    
+    async def receive(self, text_data):
+        await self.send(text_data=f"You said: {text_data}")
